32d72d4ba5fca87b6bda326a5ad6ce12ad3a6648
Melvin Renner <melvin.renner@googlemail.com>
2021-04-01 13:59:35 +0200

    first commit

---
+++ Pretrainer/pretrainerlatesplit.py
@@ -0,0 +1,200 @@
+import numpy as np
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import Adam, SGD
+import torchvision
+from torchvision import transforms
+import torch
+
+batchsize = 64
+epoch = 250
+error = nn.MSELoss()
+lr = 0.0001
+
+
+# Create Transforms
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+
+# Create Datasets
+subsetA, subsetB = torch.utils.data.random_split(trainset, [round(0.2*len(trainset)), len(trainset)-round(0.2*len(trainset))])
+
+# Load Datasets
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=batchsize, shuffle=True, num_workers=2)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=1500, shuffle=False, num_workers=2)
+total_batch = len(train_loader)
+
+
+
+class Encode(nn.Module):
+    """
+    encoder model
+    """
+    def __init__(self):
+        super(Encode, self).__init__()
+        self.conva = nn.Conv2d(64, 16, 3, padding=1)
+        self.convb = nn.Conv2d(16, 8, 3, padding=1)
+        self.convc = nn.Conv2d(8, 4, 3, padding=1)##
+
+    def forward(self, x):
+        x = self.conva(x)
+        x = self.convb(x)
+        x = self.convc(x)
+        return x
+
+class Decode(nn.Module):
+    """
+    decoder model
+    """
+    def __init__(self):
+        super(Decode, self).__init__()
+        self.t_convx = nn.ConvTranspose2d(4, 8, 1, stride=1)
+        self.t_conva = nn.ConvTranspose2d(8, 16, 1, stride=1)
+        self.t_convb = nn.ConvTranspose2d(16, 64, 1, stride=1)
+
+    def forward(self, x):
+        x = self.t_convx(x)
+        x = self.t_conva(x)
+        x= self.t_convb(x)
+        return x
+
+
+# just the part of the clientmidel before the autoencoder
+class Client(nn.Module):
+    """
+    client model
+    """
+    def __init__(self):
+        super(Client, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.norm1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.relu2 = nn.ReLU()
+        self.norm2 = nn.BatchNorm2d(32)
+        self.drop1 = nn.Dropout2d(0.2)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1))
+        self.relu3 = nn.ReLU()
+        self.norm3 = nn.BatchNorm2d(64)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu3(x)
+        x = self.norm2(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.norm3(x)
+        return x
+
+
+def start_training():
+    """
+    function, which does the training process for the autoencoder and seves the weights at the end of every epoch,
+    if no divergenvce occurs
+
+    --> adaptive learning rate, depending on divergences --> in case of divergence, results of the epoch are dismissed,
+    and the training of this epoch restarts with a smaller learning rate
+
+    """
+    for c, batch_test in enumerate(test_loader):
+        x_tester, label_tester = batch_test
+        break
+    prevloss = 999
+    learnrate = lr###
+    optimizerencode = Adam(encode.parameters(), lr=learnrate)###
+    optimizerdecode = Adam(decode.parameters(), lr=learnrate)###
+
+    for e in range(epoch):
+        x_test, label_test = x_tester, label_tester
+        x_test, label_test = x_test.to(device), label_test.to(device)
+        for b, batch in enumerate(train_loader):
+                x_train, label_train = batch
+                x_train, label_train = x_train.to(device), label_train.to(device)
+                #print(x_train.shape)
+                x_train = client(x_train)
+                output_train = encode(x_train)
+                output_train = decode(output_train)
+
+                optimizerencode.zero_grad()
+                optimizerdecode.zero_grad()
+                loss_train = error(x_train, output_train)  # calculates cross-entropy loss
+                loss_train.backward()
+
+                optimizerencode.step()
+                optimizerdecode.step()
+
+        with torch.no_grad():
+            x_test = client(x_test)
+            output_test = encode(x_test)
+            output_test = decode(output_test)
+            loss_test = error(x_test, output_test).data
+        print("epoch: {}, train-loss: {:.6f}, test-loss: {:.6f} ".format(e + 1,  loss_train, loss_test))
+
+
+        if loss_test <= prevloss*1.1:
+            torch.save(encode.state_dict(), "./convencoder.pth")
+            encode2 = Encode()
+            encode2.load_state_dict(torch.load("./convencoder.pth"))
+            encode2.eval()
+
+            torch.save(decode.state_dict(), "./convdecoder.pth" )
+            prevloss = loss_test
+
+        else:
+            print ("diverge")
+            encode.load_state_dict(torch.load("./convencoder.pth"))
+            encode.eval()
+            encode.to(device)
+            decode.load_state_dict(torch.load("./convdecoder.pth"))
+            decode.eval()
+            decode.to(device)
+            #break
+            learnrate = 0.5*learnrate
+            print(learnrate)
+            optimizerencode = Adam(encode.parameters(), lr=learnrate)###
+            optimizerdecode = Adam(decode.parameters(), lr=learnrate)###
+
+
+def main():
+    """
+    initialize device, client model, encoder and decoder and starts the training process
+    """
+
+    global device
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # cuda:0
+    seed = 0
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    global client
+    client = Client()
+    client.to(device)
+
+    global encode
+    encode = Encode()
+    encode.to(device)
+
+    global decode
+    decode = Decode()
+    decode.to(device)
+
+    start_training()
+
+
+if __name__ == '__main__':
+    main()
---
+++ server/Server.py
@@ -0,0 +1,351 @@
+import socket
+import struct
+import pickle
+import numpy as np
+import json
+import torch
+import torch.nn as nn
+from torch.optim import Adam, SGD
+from threading import Thread
+import time
+
+
+f = open('parameter_server.json', )
+data = json.load(f)
+
+host = data["host"]
+port = data["port"]
+max_recv = data["max_recv"]
+lr = data["learningrate"]
+update_treshold = data["update_threshold"]
+max_numclients = data["max_nr_clients"]
+
+
+class Decode(nn.Module):
+    """
+        decoder model
+    """
+    def __init__(self):
+        super(Decode, self).__init__()
+        self.t_convx = nn.ConvTranspose2d(4, 8, 1, stride=1)
+        self.t_conva = nn.ConvTranspose2d(8, 16, 1, stride=1)
+        self.t_convb = nn.ConvTranspose2d(16, 64, 1, stride=1)
+
+    def forward(self, x):
+        x = self.t_convx(x)
+        x = self.t_conva(x)
+        x = self.t_convb(x)
+        return x
+
+
+class Server(nn.Module):
+    """
+    server model
+    """
+    def __init__(self):
+        super(Server, self).__init__()
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1))
+        self.relu4 = nn.ReLU()
+        self.norm4 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.drop2 = nn.Dropout2d(0.3)
+        self.conv5 = nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1))
+        self.relu5 = nn.ReLU()
+        self.norm5 = nn.BatchNorm2d(128)
+        self.conv6 = nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1))
+        self.relu6 = nn.ReLU()
+        self.norm6 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.drop3 = nn.Dropout2d(0.4)
+        self.linear1 = nn.Linear(in_features=128, out_features=43, bias=True)
+
+    def forward(self, x):
+        x = self.conv4(x)
+        x = self.relu4(x)
+        x = self.norm4(x)
+        x = self.pool2(x)
+        x = self.drop2(x)
+        x = self.conv5(x)
+        x = self.relu5(x)
+        x = self.norm5(x)
+        x = self.conv6(x)
+        x = self.relu6(x)
+        x = self.norm6(x)
+        x = self.pool3(x)
+        x = self.drop3(x)
+        x = x.view(x.size(0), -1)
+        x = nn.functional.log_softmax(self.linear1(x), dim=1)
+        return x
+
+
+class initial_Client(nn.Module):
+    """
+        initial client model with the actual weights
+    """
+    def __init__(self):
+        super(initial_Client, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.norm1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.relu2 = nn.ReLU()
+        self.norm2 = nn.BatchNorm2d(32)
+        self.drop1 = nn.Dropout2d(0.2)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1))
+        self.relu3 = nn.ReLU()
+        self.norm3 = nn.BatchNorm2d(64)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.norm1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.norm2(x)
+        x = self.drop1(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.norm3(x)
+        # x = encode(x)
+        return x
+
+
+def send_msg(sock, content):
+    """
+    pickles the content (creates bitstream), adds header and send message via tcp port
+
+    :param sock: socket
+    :param content: content to send via tcp port
+    """
+    msg = pickle.dumps(content)
+    msg = struct.pack('>I', len(msg)) + msg  # add 4-byte length in netwwork byte order
+    sock.sendall(msg)
+
+
+def recieve_msg(sock):
+    """
+    recieves the meassage with helper function, umpickles the message and separates the getid from the actual massage content
+    calls the request handler
+    :param sock: socket
+    """
+    msg = recv_msg(sock)  # receive client message from socket
+    msg = pickle.loads(msg)
+    getid = msg[0]
+    content = msg[1]
+
+    handle_request(sock, getid, content)
+
+
+def recv_msg(sock):
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    return recvall(sock, msglen)
+
+
+def recvall(sock, n):
+    data = b''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+
+def handle_request(sock, getid, content):
+    """
+    executes the requested function, depending on the get id, and passes the recieved message
+
+    :param sock: socket
+    :param getid: id of the function, that should be executed if the message is recieved
+    :param content: message content
+    """
+    switcher = {
+        0: calc_gradients,
+        1: get_testacc,
+        2: updateclientmodels,
+    }
+    switcher.get(getid, "invalid request recieved")(sock, content)
+
+
+def get_testacc(conn, msg):
+    """
+    this method does the forward propagation with the recieved data, from to first layer of the decoder to the last layer
+    of the model. It sends information about loss/accuracy back to the client.
+
+    :param conn: connection
+    :param msg: message
+    """
+    with torch.no_grad():
+        client_output_test, label_test = msg['client_output_test'], msg['label_test']
+        client_output_test, label_test = client_output_test.to(device), label_test.to(device)
+        client_output_test = decode(client_output_test)  #
+        client_output_test = client_output_test.clone().detach().requires_grad_(True)
+        output_test = server(client_output_test)
+        loss_test = error(output_test, label_test)
+        test_loss = loss_test.data
+        correct_test = torch.sum(output_test.argmax(dim=1) == label_test).item()
+
+    msg = {"test_loss": test_loss,
+           "correct_test": correct_test,
+           }
+    send_msg(conn, msg)
+
+
+def updateclientmodels(sock, updatedweights):
+    """
+    send the actual clientside weights to all connected clients, except from the clint that is currently training
+
+    :param sock: the socket
+    :param updatedweights: the client side weghts with actual status
+    """
+    client.load_state_dict(updatedweights)
+    for clientss in connectedclients:
+        try:
+            if clientss != sock:
+                send_msg(clientss, updatedweights)
+        except:
+            pass
+
+
+def calc_gradients(conn, msg):
+    """
+    this method does the forward propagation with the recieved data, from to first layer of the decoder to the last layer
+    of the model. it calculates the loss, and does the backward propagation up to the cutlayer of the model.
+    Depending on if the loss threshold is reached it sends the gradient of the back propagation at the cut layer and
+    information about loss/accuracy/trainingtime back to the client.
+
+    :param conn: the connected socket of the currently training client
+    :param msg: the recieved data
+    """
+    start_time_training = time.time()
+    optimizer.zero_grad()
+    with torch.no_grad():
+        client_output_train, label_train, batchsize, batch_concat = msg['client_output_train'], msg['label_train'], msg[
+            'batchsize'], msg['batch_concat']  # client output tensor
+        client_output_train, label_train = client_output_train.to(device), label_train
+        client_output_train = decode(client_output_train)
+    splittensor = torch.split(client_output_train, batchsize, dim=0)
+
+    dc = 0
+    while dc < batch_concat:
+        tenss = splittensor[dc]
+        tenss = tenss.requires_grad_(True)
+        tenss = tenss.to(device)
+
+        output_train = server(tenss)  # forward propagation
+        with torch.no_grad():
+            lbl_train = label_train[dc].to(device)
+        loss_train = error(output_train, lbl_train)  # calculates cross-entropy loss
+        train_loss = loss_train.data
+        loss_train.backward()  # backward propagation
+        client_grad = tenss.grad.clone().detach()
+        optimizer.step()
+        add_correct_train = torch.sum(output_train.argmax(dim=1) == lbl_train).item()
+        add_total_train = len(lbl_train)
+        total_training_time = time.time() - start_time_training
+
+        if train_loss.item() > update_treshold:
+            pass
+        else:
+            client_grad = "abort"
+
+        msg = {"grad_client": client_grad,
+               "train_loss": train_loss,
+               "add_correct_train": add_correct_train,
+               "add_total_train": add_total_train,
+               "active_trtime_batch_server": total_training_time,
+               }
+
+        send_msg(conn, msg)
+        dc += 1
+
+
+def initialize_client(conn):
+    """
+    called when new client connect. if new connected client is neot the first connected client, the send the initial weights to
+    the new connected client
+    :param conn:
+    """
+    print(len(connectedclients))
+    if len(connectedclients) == 1:
+        msg = 0
+    else:
+        initial_weights = client.state_dict()
+        msg = initial_weights
+    send_msg(conn, msg)
+
+
+def clientHandler(conn, addr):
+    initialize_client(conn)
+    while True:
+        try:
+            recieve_msg(conn)
+        except:
+            pass
+
+
+connectedclients = []
+trds = []
+
+
+def main():
+    """
+    initialize device, server model, initial client model, optimizer, loss, decoder and accepts new clients
+    """
+    global device
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    if (torch.cuda.is_available()):
+        print("training on gpu")
+    seed = 0
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    global server
+    server = Server()
+    server.to(device)
+
+    global client
+    client = initial_Client()
+    client.to(device)
+
+    global optimizer
+    optimizer = SGD(server.parameters(), lr=lr, momentum=0.9)
+
+    global error
+    error = nn.CrossEntropyLoss()
+
+    global optimizer_client
+    optimizer_client = SGD(client.parameters(), lr=lr, momentum=0.9)
+
+    global decode
+    decode = Decode()
+    decode.load_state_dict(torch.load("./convdecoder.pth"))
+    decode.eval()
+    decode.to(device)
+
+    s = socket.socket()
+    s.bind((host, port))
+    s.listen(max_numclients)
+
+    for i in range(max_numclients):
+        conn, addr = s.accept()
+        connectedclients.append(conn)
+        print('Conntected with', addr)
+        t = Thread(target=clientHandler, args=(conn, addr))
+        trds.append(t)
+        t.start()
+    for t in trds:
+        t.join()
+
+
+if __name__ == '__main__':
+    main()
